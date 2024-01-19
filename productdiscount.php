5ddb419bf63cf91d854629906784d66403b09c15
Dokukin Vyacheslav Olegovich <toorrp4@gmail.com>
2024-01-19 11:45:37 +0300

    Fix bug "Query Error: SELECT ID FROM b_iblock_element WHERE OR ID IN (189) AND ACTIVE = ..." - Добавлены проверки и условия перед формированием запроса к `b_iblock_element`

--- productdiscount.php
+++ productdiscount.php
@@ -41,20 +41,25 @@ class ProductDiscount
         $arOr = array();
         $date = date($DB->DateFormatToPHP('Y-m-d'), mktime(0, 0, 0, date("n"), date("d"), date("Y")));
         $results = $DB->Query("SELECT `ID`, `CONDITIONS`, `ACTIONS` FROM `b_sale_discount` WHERE `ACTIVE_TO` >= '" . $date . "' AND `ACTIVE` ='Y'");
-        
+
         while($row = $results->Fetch())
         {
             $return_value = array();
             $ACTIONS = unserialize($row['ACTIONS']);
-        
+
             foreach($ACTIONS['CHILDREN'] as $children)
             {
+
                 foreach($children['CHILDREN'] as $child)
                 {
                     if(isset($child['CHILDREN']))
                     {
                         foreach($child['CHILDREN'] as $cond)
                         {
+
+                            if(!$cond['DATA'])
+                                continue;
+
                             $return_value[] = match ($cond['CLASS_ID']) {
                                 'CondIBName' => self::CondIBName($cond['DATA']),
                                 'CondIBElement' => self::CondIBElement($cond['DATA']),
@@ -63,6 +68,10 @@ class ProductDiscount
                             };
                         }
                     }else{
+
+                        if(!$child['DATA'])
+                            continue;
+
                         $return_value[] = match ($child['CLASS_ID']) {
                             'CondIBName' => self::CondIBName($child['DATA']),
                             'CondIBElement' => self::CondIBElement($child['DATA']),
@@ -73,24 +82,26 @@ class ProductDiscount
                 }
             }
         
-            $arOr[] = implode(' OR ', $return_value);
+            if($return_value)
+                $arOr[] = implode(' OR ', $return_value);
         
         }
 
-        if(!count($arOr)){
-            return array();
-        }
-        
-        $res = $DB->Query("SELECT `ID` FROM `b_iblock_element` WHERE " . implode(' OR ', $arOr) . " AND `ACTIVE` = 'Y';");
-        
-        while($el = $res->Fetch())
+        if(count($arOr))
         {
-            self::$result[] = $el['ID'];
+            $res = $DB->Query("SELECT `ID` FROM `b_iblock_element` WHERE " . implode(' OR ', $arOr) . " AND `ACTIVE` = 'Y';");
+            
+            while($el = $res->Fetch())
+            {
+                self::$result[] = $el['ID'];
+            }
+
+            self::$result = array_unique(self::$result);
+
+            return self::$result;
         }
 
-        self::$result = array_unique(self::$result);
-
-        return self::$result;
+        return array();
     }
 }
 ?>
